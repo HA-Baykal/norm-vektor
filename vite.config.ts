99b0326caf8a32c3b4a194744dbd677f9bc863b5
Baykal <69599574+HA-Baykal@users.noreply.github.com>
2026-07-26 22:15:33 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import vitePrerender from 'vite-plugin-prerender'
-import path from 'path'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    vitePrerender({
-      staticDir: path.join(__dirname, 'dist'),
-      routes: [ '/', '/okna', '/kondicionery', '/ventilyaciya', '/almaznoe-burenie', '/standarty', '/portfolio', '/blog', '/otzyvy', '/contact' ]
-    })
-  ]
+  plugins: [react()],
 })
